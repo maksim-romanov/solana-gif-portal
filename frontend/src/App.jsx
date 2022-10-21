48d3698c27074df898117325c623914773050c23
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-10-22 00:31:58 +0400

    WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,29 +16,58 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen p-12 flex flex-col">
-      <div className="fixed top-0 left-0 p-2 opacity-85">
+    <div className="min-h-screen p-4 md:p-12 flex flex-col">
+      <div className="fixed z-50 top-0 left-0 right-0">
+        <div className="bg-indigo-600 px-4 py-3 text-white">
+          <p className="text-center text-sm font-medium">Solana Devnet</p>
+        </div>
+      </div>
+      {/* <div className="fixed top-2 left-2 opacity-85 z-50 shadow-md rounded-full">
         <a
           href="#"
-          className="text-xs inline-flex justify-between items-center px-1 pr-4 text-gray-700 bg-gray-100 rounded-full  hover:bg-gray-200"
+          className="text-xs inline-flex justify-between items-center px-1 pr-4 text-gray-700 bg-white rounded-full  hover:bg-gray-200"
           role="alert"
         >
-          <span className=" bg-purple-500 rounded-full text-white px-4 py-1 mr-3">
+          <span className=" bg-black rounded-full text-white px-4 py-1 mr-3">
             <span className="animate-pulse">Devnet</span>
           </span>
           <span className="font-medium">Phantom Wallet</span>
         </a>
-      </div>
+      </div> */}
 
-      <section className="max-h-screen p-12 flex flex-col items-center">
-        <div className="min-h-full flex flex-col justify-center items-center space-y-4">
-          <h1 className=" text-5xl font-bold capitalize text-slate-700">🖼 Solana gif portal</h1>
+      <section className="text-black flex justify-center">
+        <div className="w-full max-w-screen-xl py-32 lg:flex lg:items-center flex justify-center">
+          <div className="max-w-3xl text-center flex flex-col justify-center items-center space-y-12 w-full">
+            <div className="flex flex-col justify-center items-center space-y-3">
+              <h1 className="text-5xl font-extrabold text-center">
+                <span className="text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-blue-500 to-purple-600">
+                  Solana Gif Portal
+                </span>
+              </h1>
 
-          <span className=" text-xl text-slate-800">View your GIF collection in the metaverse ✨</span>
-        </div>
+              <p className="max-w-xl sm:text-xl sm:leading-relaxed">View your GIF collection in the metaverse ✨</p>
+            </div>
 
-        <div>
-          <GifLinkForm onSubmit={onSubmit} />
+            <div className="w-full max-w-2xl flex flex-col space-y-8 sm:space-y-0 sm:flex-row justify-between items-center sm:items-stretch space-x-0 sm:space-x-3 group">
+              {/* <GifLinkForm onSubmit={onSubmit} /> */}
+
+              <input
+                type="text"
+                name="giflink"
+                id="giflink"
+                className="outline-0 bg-white border-2 border-black text-gray-900 sm:text-lg rounded-xl group-focus-within:ring-primary-600 group-focus-within:border-primary-600 block w-full p-2.5 pr-42 shadow-[6px_6px_0_0_#000] transition focus:shadow-none focus:border-slate-700 focus:ring ring-purple-500/50"
+                placeholder="Gif link here to metaverse here ..."
+                required=""
+              />
+
+              <button className="flex items-center justify-center rounded-xl border-2 border-black px-6 py-2 font-bold shadow-[6px_6px_0_0_#000] transition hover:shadow-none focus:outline-none focus:ring ring-purple-500/50 active:bg-purple-200/50 max-w-xs">
+                Mint
+                <span aria-hidden="true" className="ml-1.5" role="img">
+                  🚀
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
       </section>
 
--- frontend/src/components/shared/ImagePreview/ImagePreview.jsx
+++ frontend/src/components/shared/ImagePreview/ImagePreview.jsx
@@ -4,33 +4,29 @@ import Likes from './Likes';
 
 function ImagePreview({ item }) {
   return (
-    <div className="relative h-full">
-      <div className=" bg-black absolute top-0 right-0 bottom-0 left-0 border border-dashed border-black rounded-lg translate-x-1.5 translate-y-1.5 -z-10" />
+    <div className="h-full border-2 border-black rounded-xl p-2 py-0 flex flex-col z-10 bg-white shadow-[6px_6px_0_0_#000]">
+      <div className="py-2 px-1 flex justify-between items-center">
+        <img
+          className="h-8 w-8 rounded-full border border-stone-200"
+          src="https://avatars.githubusercontent.com/t/6813046?s=280&v=4"
+        />
 
-      <div className="h-full border-2 border-black rounded-lg p-2 py-0 flex flex-col z-10 bg-white">
-        <div className="py-2 px-1 flex justify-between items-center">
-          <img
-            className="h-8 w-8 rounded-full border border-stone-200"
-            src="https://avatars.githubusercontent.com/t/6813046?s=280&v=4"
-          />
-
-          <div className="truncate pl-8">
-            <span>CtR8U3nwdnHHuV8VStsuvM9e9Fzb3j3cn8fmcu6tQJMt</span>
-          </div>
+        <div className="truncate pl-8">
+          <span>CtR8U3nwdnHHuV8VStsuvM9e9Fzb3j3cn8fmcu6tQJMt</span>
         </div>
+      </div>
 
-        <img className="w-full h-full max-h-64 rounded-lg border border-stone-200" src={item.url} />
+      <img className="w-full h-full max-h-64 rounded-xl border border-stone-200" src={item.url} />
 
-        <div className="py-2 px-1 flex justify-between items-center space-x-2">
-          <Likes value={12} />
+      <div className="py-2 px-1 flex justify-between items-center space-x-2">
+        <Likes value={12} />
 
-          <button className="text-green-600 flex justify-between items-center">
-            <span className="text-xs">
-              {/* Donate <span className=" font-bold">0.05</span> SOL */}
-              Donate
-            </span>
-          </button>
-        </div>
+        <button className="text-green-600 flex justify-between items-center">
+          <span className="text-xs">
+            {/* Donate <span className=" font-bold">0.05</span> SOL */}
+            Donate
+          </span>
+        </button>
       </div>
     </div>
   );
