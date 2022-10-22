7f51f947a74372e363eb760b203ac318825106fd
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-10-22 21:12:12 +0400

    Fixed vite path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,8 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+
   resolve: {
     alias: {
       components: path.resolve(__dirname, './src/components'),
